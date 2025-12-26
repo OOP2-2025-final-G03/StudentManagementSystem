9e11d2dd9cfc9ac0c046c426f543f871a7ddd9b4
告白 <kout5504@gmail.com>
2025-12-26 21:27:27 +0900

    テンプレート

---
+++ config.py
@@ -0,0 +1,31 @@
+"""
+アプリケーションの設定
+"""
+import os
+from datetime import timedelta
+
+
+class Config:
+    """
+    アプリケーション全体の設定クラス。
+
+    flaskのappに読み込んだはずです、current_app.config["KEY"]でアクセスできます。
+    使用する際にfrom flask import current_appで導入してください。
+    """
+
+    # アプリケーション名
+    APP_NAME = "学生管理システム"
+
+    # セッション管理用の秘密鍵（開発時のみ使用）
+    SECRET_KEY = "secret-key-for-development"
+
+    HOST = os.getenv("HOST", "0.0.0.0") # ホスト名
+    PORT = int(os.getenv("PORT", "8000")) # ポート番号
+
+    # デバッグモード（開発時のみ True）
+    DEBUG = True
+
+    # セッション設定
+    SESSION_PERMANENT = False # セッションが期限切れになった後でも保持するかどうか
+    PERMANENT_SESSION_LIFETIME = timedelta(minutes=60) # セッションの有効期限（分）
+
--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from config import Config
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template("index.html")
 
 @app.route('/login')
 def login():
-    return 'Login Page'
+    return render_template("login.html")
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host=Config.HOST, port=Config.PORT, debug=Config.DEBUG)
